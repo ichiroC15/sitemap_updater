40f524262ed7d2c2b01f0533b7aecb946b748246
IchiroC15 <ichiro.n.c15@gmail.com>
2021-02-02 11:19:02 +0900

    execution method implemented refs #7

--- lib/sitemap_updater.rb
+++ lib/sitemap_updater.rb
@@ -3,5 +3,40 @@ require "sitemaps/creator"
 require "sitemapindex/updater"
 
 module SitemapUpdater
+  class Cont
+    # @params(Str, Str)
+    # [Str]the absolute path of the directory where sitemaps locates
+    # [Str]the name of the sitemap file
+    def initialize(sitemaps_dir, sitemap_name)
+      @start_time   = Time.now
+      @sitemaps_dir = sitemaps_dir
+      @sitemap_name = sitemap_name
+    end
+
+    # create the sitemap files & update the sitemapindex file
+    # @params(Arr, Str, Str, Str, Str)
+    # [Arr]URLs to be included in the sitemap file
+    # [Str]the frequency the URLs changes,
+    #      select from "always", "hourly", "daily", "weekly", "monthly", "yearly", "never"
+    # [Str]the priority of the URLs, select from "0.0" ~ "1.0"
+    def update(urls, sitemapindex_path, toppage_url, freq = nil, prio = nil)
+      conds         = { "frequency" => freq, "priority" => prio }
+      sitemap_paths = Sitemaps::Creator.create_sitemaps(@start_time, @sitemaps_dir, @sitemap_name, urls, conds)
+      Sitemapindex::Updater.
+        update_sitemapindex(@start_time, sitemapindex_path, sitemap_name, sitemap_paths, toppage_url)
+    end
+
+    # only create the sitemap files, not update the sitemapindex file
+    # @params(Arr, Str, Str)
+    # [Arr]URLs to be included in the sitemap file
+    # [Str]the frequency the URLs changes,
+    #      select from "always", "hourly", "daily", "weekly", "monthly", "yearly", "never"
+    # [Str]the priority of the URLs, select from "0.0" ~ "1.0"
+    def only_create_sitemaps(urls, freq = nil, prio = nil)
+      conds = { "frequency" => freq, "priority" => prio }
+      Sitemaps::Creator.create_sitemaps(@sitemaps_dir, @sitemap_name, urls, conds)
+    end
+  end
+
   class Error < StandardError; end
 end
--- spec/sitemap_updater_spec.rb
+++ spec/sitemap_updater_spec.rb
@@ -1,9 +1,62 @@
+require "spec_helper"
+require "pry-byebug"
+require "timecop"
+
 RSpec.describe SitemapUpdater do
-  it "has a version number" do
-    expect(SitemapUpdater::VERSION).not_to be nil
+  describe "SitemapUpdater::Cont" do
+    let!(:time_freeze) { Timecop.freeze("2020-01-01") }
+    let(:cont) { SitemapUpdater::Cont.new(dir, name) }
+    let(:dir)        { "/home/sitemaps/"}
+    let(:name)       { "sitemap_test" }
+
+    describe "#initialize(sitemaps_dir, sitemap_name)" do
+      subject(:result) { cont }
+      it "has @start_time & @sitemaps_dir & @sitemap_name" do
+        expect(result.instance_variable_get(:@start_time)).to eq(Time.now)
+        expect(result.instance_variable_get(:@sitemaps_dir)).to eq("/home/sitemaps/")
+        expect(result.instance_variable_get(:@sitemap_name)).to eq("sitemap_test")
+      end
+    end
+
+    describe "#update(urls, sitemapindex_path, toppage_url, freq, prio)" do
+      let!(:set_mock) do
+        allow(Sitemaps::Creator).to receive(:create_sitemaps).with(time, dir, name, urls, conds)
+        allow(Sitemapindex::Updater).to receive(:update_sitemapindex).with(time, path, name, paths, url)
+      end
+      subject(:result) { cont.update(urls, path, url, freq, prio) }
+      let(:time)  { Time.now }
+      let(:urls)  { ["url0", "url1", "url2"] }
+      let(:path)  { "spec/sitemap.xml.gz" }
+      let(:url)   { "http://www.example.com/" }
+      let(:freq)  { "daily" }
+      let(:prio)  { "0.5" }
+      let(:conds) { { "frequency" => freq, "priority" => prio } }
+      let(:paths) { ["/home/sitemaps/sitemap_test.xml.gz"] }
+      it "calls #create_sitemaps" do
+        allow(Sitemaps::Creator).to receive(:create_sitemaps).with(time, dir, name, urls, conds).once
+      end
+      it "calls #update_sitemapindex" do
+        allow(Sitemapindex::Updater).to receive(:update_sitemapindex).with(time, path, name, paths, url).once
+      end
+    end
+
+    describe "#only_create_sitemaps(urls, freq, prio)" do
+      let!(:set_mock) do
+        allow(Sitemaps::Creator).to receive(:create_sitemaps).with(time, dir, name, urls, conds)
+      end
+      subject(:result) { cont.only_create_sitemaps(urls, freq, prio) }
+      let(:time)  { Time.now }
+      let(:urls)  { ["url0", "url1", "url2"] }
+      let(:conds) { { "frequency" => freq, "priority" => prio } }
+      let(:freq)  { "daily" }
+      let(:prio)  { "0.5" }
+      it "calls #create_sitemaps" do
+        allow(Sitemaps::Creator).to receive(:create_sitemaps).with(time, dir, name, urls, conds).once
+      end
+    end
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "has a version number" do
+    expect(SitemapUpdater::VERSION).not_to be nil
   end
 end
